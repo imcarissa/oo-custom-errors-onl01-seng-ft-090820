1979faa4760872f62137e2f3f0421c7d295e9aac
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-28 00:21:36 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,9 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person
+    if person.class != person
+      raise PartnerError
+    else
     person.partner = self
   end
   
